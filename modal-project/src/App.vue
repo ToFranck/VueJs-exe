9766e3492ba19704e41a150ae708e04520edcacf
Franck Tolosano <f.tolosano@nanaba.tech>
2022-05-10 12:02:29 +0200

    modif files

---
+++ modal-project/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome..</p>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">Click me</button> -->
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div> <br>
+  <button @click="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+
+export default {
+  name: 'App',
+  components:{ Modal },
+  data(){
+    return{
+      title: 'Ma Premere Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'oui oui oui oui ',
+      showModal: false,
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4e5154;
+  margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display:inline-block;
+  padding-bottom: 10px;
+}
+.modal.sale {
+  background: red;
+  color: white;
+}
+.modal.sale h1{
+  color: white;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="backdrop" @click="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale'}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['header', 'text', 'theme'],
+    methods :{
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+
+}
+
+</script>
+
+<style>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgb(218, 218, 218);
+        width: 100%;
+        height: 100%;
+        
+    }
+    h1{
+        color: aqua;
+    }
+    
+</style>
